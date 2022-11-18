84c873d63ffe01828d6a9616a389f93b8fc9e86d
Your Name <you@example.com>
2022-11-18 13:43:44 +0100

    Updated

--- base.sh
+++ base.sh
@@ -7,7 +7,7 @@ sudo pacman -Syu
 
 #Sys utils
 echo "[*]Get some sys utils"
-sudo pacman -S  usbutils curl thunar spice-vdagent thunar-volman gfvs gvim remmina proxychains git coreutils feh flameshot iptables terminator bluez bluez-utils blueman unzip htop lshw pulseaudio alsa-utils pavucontrol xfce4-power-manager brightnessctl vlc libreoffice xf86-video-qxl
+sudo pacman -S  usbutils curl thunar spice-vdagent thunar-volman gvfs gvim remmina proxychains git coreutils feh flameshot iptables terminator bluez bluez-utils blueman unzip htop lshw pulseaudio alsa-utils pavucontrol xfce4-power-manager brightnessctl vlc libreoffice xf86-video-qxl 
 
 # Setting up locales
 localectl set-keymap --no-convert fr-latin1
